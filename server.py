ca7b6b3ca7fc3019fb7d6d0ad3ebe63fde79666e
Francis Kogge <koggefa@gmail.com>
2022-11-03 16:48:19 -0700

    mock api for getting makefile

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, send_file, jsonify
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello_world():
+    print('in hello')
+    return jsonify(
+        {
+            'response': 'hello world'
+        }
+    )
+
+@app.route('/makefile/<name>')
+def get_makefile(name):
+    print('in /makefile')
+    path = './responses/{}'.format(name)
+    return send_file(path)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=55321)
