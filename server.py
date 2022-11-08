5890fcec9ef24334f44d2bc022e960d0e11501e2
Francis Kogge <koggefa@gmail.com>
2022-11-07 16:51:28 -0800

    change to canvas file mock api

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, send_file, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/hello", methods=['GET'])
 def hello_world():
     print('in hello')
@@ -11,15 +12,18 @@ def hello_world():
         }
     )
 
+
 @app.route('/makefile/<name>', methods=['GET'])
 def get_makefile(name):
     print('in /makefile')
     path = './responses/{}/makefile'.format(name)
     return send_file(path)
 
+
+# Same endpoint as Canvas API
 @app.route('/api/v1/files/<fileId>', methods=['GET'])
 def get_canvas_file(fileId):
-    path = './responses/canvas/get-file.json'
+    path = 'responses/canvas/get-file/{}.json'.format(fileId)
     return send_file(path)
 
 if __name__ == '__main__':
